f4a3077caa07983fcaf190e68aed7e0592be6dab
courtney <65988246+cbolton56@users.noreply.github.com>
2025-03-24 03:03:06 +0000

    add function test

--- tests/unit/test_city_matcher.py
+++ tests/unit/test_city_matcher.py
@@ -1,15 +1,13 @@
 import pytest
 from CityAutoCompleter.CityAutoCompleter import CityAutoCompleter
 
-def test_city_search_returns_chicago():
+def test_city_search_with_no_optional_params():
     city_search = CityAutoCompleter()
     results = city_search.suggest("chi")
-    print(results)
-    # assert results[0]['name'] == "Chicago"
+    assert results[0]['name'] == "Chino"
 
-def test_city_search_with_lat_long_returns_chicago():
+def test_city_search_with_lat_long():
     city_search = CityAutoCompleter()
     results = city_search.suggest("chi", user_lat=41.84, user_lon=-87.65)
-    print(results)
     assert results[0]['name'] == "Chicago"
 
